38098b299743ca4582cbf11a4be40f7ebb66d57e
wesleyhuang23 <wesleyhuang23@gmail.com>
2017-03-16 18:42:29 -0700

    mocha battle ship example working

--- test/main_test.js
+++ test/main_test.js
@@ -3,7 +3,7 @@ var expect = require('chai').expect;
 //Test Suite
 //tests similar functions
 //write a sanity check
-describe('Mocha', function(){
+describe('Mocha', function(){ //describe what you are checking in first string
     //Test spec (unit test) contains in it()
     it('should run our tests using npm', function(){
         expect(1 === 1).to.be.ok; //tests if value is truthy
